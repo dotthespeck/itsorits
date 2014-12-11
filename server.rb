e4f5882403cc22df2a9547e5faecf9d9c212cb07
Zoe <doticus@gmail.com>
2014-12-10 22:37:23 -0500

    Working version of the app!

---
+++ server.rb
@@ -0,0 +1,29 @@
+require 'sinatra'
+require 'pry'
+require 'sinatra/contrib'
+require 'sanitize'
+
+get '/' do
+
+erb :index
+end
+
+post '/create_sentence' do
+
+old_sentence = params[:old_sentence]
+@old_sentence = Sanitize.clean(old_sentence)
+@new_sentence = @old_sentence.to_s
+
+erb :new_sentence
+
+end
+
+get '/yes' do
+
+  erb :yes
+end
+
+get '/no' do
+
+ erb :no
+end
